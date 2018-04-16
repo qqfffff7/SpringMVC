8ae178efabd4822c63a943d2912011f335e4b8c4
qqfffff7 <562614633@qq.com>
2018-04-16 15:02:10 +0800

    Initial commit

---
+++ src/priv/xxy/controller/AppController.java
@@ -0,0 +1,16 @@
+package priv.xxy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2018/4/16/016.
+ */
+@Controller
+public class AppController {
+
+    @RequestMapping
+    public String test(){
+        return null;
+    }
+}
---
+++ src/priv/xxy/dao/UserDao.java
@@ -0,0 +1,8 @@
+package priv.xxy.dao;
+
+/**
+ * Created by Administrator on 2018/4/16/016.
+ */
+public class UserDao {
+
+}
